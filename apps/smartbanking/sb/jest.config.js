3708ab751deb03784f52d2fbaa054d5041d33cfa
Nephi Szvoboda <nephi.szvoboda@gmail.com>
2020-11-17 16:21:46 +0100

    Move sb into smartbanking

--- apps/smartbanking/sb/jest.config.js
+++ apps/smartbanking/sb/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  displayName: 'sb',
-  preset: '../../jest.preset.js',
+  displayName: 'smartbanking-sb',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -14,7 +14,7 @@ module.exports = {
       },
     },
   },
-  coverageDirectory: '../../coverage/apps/sb',
+  coverageDirectory: '../../../coverage/apps/smartbanking/sb',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
